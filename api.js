e538ddebf8f78b5844dbbb0181cb3dd31a652967
lokeshrajarora <lokesh.slg06@gmail.com>
2018-04-03 00:36:57 +0530

    [update] add update for static file serving with express

--- api.js
+++ api.js
@@ -6,52 +6,57 @@ const morgan = require("morgan");
 const path = require("path");
 const routeMap = require("./utils/apiConfig");
 const process = require("process");
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 class Api {
-    constructor() {
-        this.app = express();
-        this.baseDir = __dirname + "/data/";
-        this.port = process.env.PORT || 3000;
-    }
+  constructor() {
+    this.app = express();
+    this.baseDir = __dirname + "/data/";
+    this.port = process.env.PORT || 3000;
+  }
 
-    initRoutes() {
-        this.app.use(morgan(":method :url :status :response-time ms"));
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({
-            extended: true
-        }));
-        this.app.use((req,res,next) => {
-            res.setHeader('Access-Control-Allow-Origin', '*');
-            next();
-        })
-        this.app.get("/health", (req,res) => {
-            res.json({
-                body: "Running all fine tbh"
-            });
-        });
-        _.map(routeMap(), v => {
-            this.genericRoute(v.route,v.fileName)
-        });
-        this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-        this.app.get("/*", (req,res) => {
-            res.sendFile(path.join(__dirname,"./build/",req.url))
-        })
-        this.app.listen(this.port, () => {
-            console.log(`Server Started at ${this.port}`);
-        })
-    }
+  initRoutes() {
+    this.app.use(morgan(":method :url :status :response-time ms"));
+    this.app.use(bodyParser.json());
+    this.app.use(
+      bodyParser.urlencoded({
+        extended: true
+      })
+    );
+    this.app.use(express.static(path.join(__dirname, "build")));
+    this.app.use((req, res, next) => {
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      next();
+    });
+    this.app.get("/health", (req, res) => {
+      res.json({
+        body: "Running all fine tbh"
+      });
+    });
+    _.map(routeMap(), v => {
+      this.genericRoute(v.route, v.fileName);
+    });
+    this.app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    this.app.get("/*", (req, res) => {
+      res.sendFile(path.join(__dirname, "./build/index.html"));
+    });
+    this.app.listen(this.port, () => {
+      console.log(`Server Started at ${this.port}`);
+    });
+  }
 
-    genericRoute(endpoint, fileName){
-        this.app.get(endpoint,(req,res) => {
-            const src = JSON.parse(fs.readFileSync(this.baseDir + fileName , {
-                encoding: "UTF-8"
-            }));
-            res.status(200);
-            res.json(src);
-        });
-    }
+  genericRoute(endpoint, fileName) {
+    this.app.get(endpoint, (req, res) => {
+      const src = JSON.parse(
+        fs.readFileSync(this.baseDir + fileName, {
+          encoding: "UTF-8"
+        })
+      );
+      res.status(200);
+      res.json(src);
+    });
+  }
 }
 
 const apiIns = new Api();
